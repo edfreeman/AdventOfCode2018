78364089fa7e178f15a6eaaa34db8f64809644d6
Ed Freeman <Ed.Freeman@endjin.com>
2018-12-04 09:04:39 +0000

    Add project files.

---
+++ Day01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.Assert(CalculateFrequency(new int[] { 1, 1, 1 }) == 3);
+            Debug.Assert(CalculateFrequency(new int[] { 1, 1, -2 }) == 0);
+            Debug.Assert(CalculateFrequency(new int[] { -1, -2, -3}) == -6);
+
+            string input = "C:/_Projects/Training/AdventOfCode/2018/AoC_2018/Day01/input.txt";
+            string[] myList = File.ReadAllLines(input);
+
+            var myListAsInts = myList.Select(x => int.Parse(x));
+            
+            Console.WriteLine(CalculateFrequency(myListAsInts));
+
+        }
+
+        static int CalculateFrequency(IEnumerable<int> values)
+        {
+            return values.Sum();
+        }
+    }
+}
