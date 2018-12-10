d6feff4e23d9ba42d74c85894f77f08dac4e6672
Ed Freeman <Ed.Freeman@endjin.com>
2018-12-10 22:36:32 +0000

    Completed Day 04

--- Day04/GuardStats.cs
+++ Day04/GuardStats.cs
@@ -8,9 +8,11 @@ namespace Day04
 {
     class GuardStats
     {
-        int Id { get; set; }
-        int MinutesAsleep { get; set; }
+        public int Id { get; set; }
+        public int MinutesAsleep { get; set; }
+        public (int? minute, int? frequency) FavouriteMinute { get; set; }
         SortedDictionary<DateTime, string> Records { get; set; }
+        List<Dictionary<string, List<DateTime>>> Shifts { get; set; }
 
         public GuardStats(KeyValuePair<int, SortedDictionary<DateTime, string>> guardRecords)
         {
@@ -18,9 +20,109 @@ namespace Day04
             Records = guardRecords.Value;
         }
 
+        public void GenerateShifts()
+        {
+            var shifts = new List<Dictionary<string, List<DateTime>>>();
+
+            var recordsAsList = Records.ToList();
+
+            for (int i = 0; i < Records.Count; i++)
+            {
+                var currentRecord = recordsAsList[i];
+
+                if (currentRecord.Value.Contains('#'))
+                {
+                    shifts.Add(new Dictionary<string, List<DateTime>>() { { "Shift start", new List<DateTime>() { currentRecord.Key } } });
+                }
+
+                if (currentRecord.Value.Equals("falls asleep"))
+                {
+                    if(!shifts.Last().ContainsKey("Falls asleep"))
+                    {
+                        shifts.Last().Add("Falls asleep", new List<DateTime>() { currentRecord.Key });
+                    }
+                    else
+                    {
+                        shifts.Last()["Falls asleep"].Add(currentRecord.Key);
+                    }
+                }
+                
+                if (currentRecord.Value.Equals("wakes up"))
+                {
+                    if (!shifts.Last().ContainsKey("Wakes up"))
+                    {
+                        shifts.Last().Add("Wakes up", new List<DateTime>() { currentRecord.Key });
+                    }
+                    else
+                    {
+                        shifts.Last()["Wakes up"].Add(currentRecord.Key);
+                    }
+                }                
+            }
+            Shifts = shifts;
+        }
+
         public int CalculateMinutesAsleep()
         {
-            return Records.Count;
+            GenerateShifts();
+
+            if(!Shifts.Any(shift => shift.Keys.Contains("Falls asleep")))
+            {
+                return MinutesAsleep = 0;
+            }
+
+            foreach(var shift in Shifts)
+            {
+                DateTime shiftStart = shift["Shift start"][0];
+                int numberOfSnoozes = shift["Falls asleep"].Count;
+
+                for (int i = 0; i < numberOfSnoozes; i++)
+                {
+                    TimeSpan snoozePeriod = shift["Wakes up"][i].Subtract(shift["Falls asleep"][i]);
+                    
+                    MinutesAsleep += (int)snoozePeriod.TotalMinutes;
+                }
+            }
+
+            return MinutesAsleep;
+        }
+
+        public (int?, int?) FindFavouriteMinute()
+        {
+            if (!Shifts.Any(shift => shift.Keys.Contains("Falls asleep")))
+            {
+                return FavouriteMinute = (null, null);
+            }
+
+            var minuteCounts = new Dictionary<int, int>();
+
+            for (int i = 0; i < 60; i++)
+            {
+                minuteCounts.Add(i, 0);
+            }
+
+            foreach(var shift in Shifts)
+            {
+                int numberOfSnoozes = shift["Falls asleep"].Count;
+
+                for (int i = 0; i < numberOfSnoozes; i++)
+                {
+                    int startMinute = shift["Falls asleep"][i].Minute;
+                    TimeSpan snoozePeriod = shift["Wakes up"][i].Subtract(shift["Falls asleep"][i]);
+                    int lengthOfSnooze = (int)snoozePeriod.TotalMinutes;
+
+                    for (int j = 0; j < lengthOfSnooze; j++)
+                    {
+                        minuteCounts[j + startMinute]++;
+                    }
+                }
+            }
+
+            var favouriteMinute = minuteCounts.OrderByDescending(minute => minute.Value).First();
+
+            FavouriteMinute = (favouriteMinute.Key, favouriteMinute.Value);
+
+            return FavouriteMinute;
         }
     }
 }
--- Day04/Program.cs
+++ Day04/Program.cs
@@ -13,15 +13,36 @@ namespace Day04
             string[] testInput = File.ReadAllLines(@"C:\_Projects\Training\AdventOfCode\2018\Day04\testInput.txt");
             string[] rawInput = File.ReadAllLines(@"C:\_Projects\Training\AdventOfCode\2018\Day04\input.txt");
 
-            SortedDictionary<DateTime, string> recordsInOrder = OrganizeRecords(testInput);
+            SortedDictionary<DateTime, string> recordsInOrder = OrganizeRecords(rawInput);
             Dictionary<int, SortedDictionary<DateTime, string>> guardsSeparated = SeparateGuards(recordsInOrder);
 
             var allGuardStats = new List<GuardStats>();
 
             foreach(var guard in guardsSeparated)
             {
-                allGuardStats.Add(new GuardStats(guard));
+                var guardStats = new GuardStats(guard);
+
+                guardStats.CalculateMinutesAsleep();
+                guardStats.FindFavouriteMinute();
+
+                allGuardStats.Add(guardStats);
             }
+
+            //Part 1
+
+            GuardStats sleepiestGuard = allGuardStats.OrderByDescending(guard => guard.MinutesAsleep).First();
+
+            int day04Part1Answer = sleepiestGuard.Id * (int)sleepiestGuard.FavouriteMinute.minute;
+
+            Console.WriteLine("Answer to day 4 part 1: " + day04Part1Answer);
+
+            //Part 2
+
+            GuardStats mostConsistentGuard = allGuardStats.OrderByDescending(guard => guard.FavouriteMinute.frequency).First();
+
+            int day04Part2Answer = mostConsistentGuard.Id * (int)mostConsistentGuard.FavouriteMinute.minute;
+
+            Console.WriteLine("Answer to day 4 part 2: " + day04Part2Answer);
         }
         
         static SortedDictionary<DateTime, string> OrganizeRecords(string[] records)
