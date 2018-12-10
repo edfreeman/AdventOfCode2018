3e61eee16688bae489a3515c62a5b8c9fcc8db26
Ed Freeman <Ed.Freeman@endjin.com>
2018-12-10 15:20:33 +0000

    Started Day 04

---
+++ Day04/GuardStats.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+
+namespace Day04
+{
+    class GuardStats
+    {
+        int Id { get; set; }
+        int MinutesAsleep { get; set; }
+        SortedDictionary<DateTime, string> Records { get; set; }
+
+        public GuardStats(KeyValuePair<int, SortedDictionary<DateTime, string>> guardRecords)
+        {
+            Id = guardRecords.Key;
+            Records = guardRecords.Value;
+        }
+
+        public int CalculateMinutesAsleep()
+        {
+            return Records.Count;
+        }
+    }
+}
---
+++ Day04/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+
+namespace Day04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] testInput = File.ReadAllLines(@"C:\_Projects\Training\AdventOfCode\2018\Day04\testInput.txt");
+            string[] rawInput = File.ReadAllLines(@"C:\_Projects\Training\AdventOfCode\2018\Day04\input.txt");
+
+            SortedDictionary<DateTime, string> recordsInOrder = OrganizeRecords(testInput);
+            Dictionary<int, SortedDictionary<DateTime, string>> guardsSeparated = SeparateGuards(recordsInOrder);
+
+            var allGuardStats = new List<GuardStats>();
+
+            foreach(var guard in guardsSeparated)
+            {
+                allGuardStats.Add(new GuardStats(guard));
+            }
+        }
+        
+        static SortedDictionary<DateTime, string> OrganizeRecords(string[] records)
+        {
+            var organizedRecords = new SortedDictionary<DateTime, string>();
+
+            foreach(var record in records)
+            {
+                var recordParts = record.TrimStart('[').Replace("Guard ","").Split("] ");
+                organizedRecords.Add(DateTime.Parse(recordParts[0]), recordParts[1]);
+            }
+
+            return organizedRecords;
+        }
+
+        static Dictionary<int, SortedDictionary<DateTime, string>> SeparateGuards(SortedDictionary<DateTime, string> records)
+        {
+            var guardsSeparated = new Dictionary<int, SortedDictionary<DateTime, string>>();
+
+            var recordsAsList = records.ToList();
+
+            int recordsToNextId = 0;            
+
+            for (int i = 0; i < recordsAsList.Count; i += recordsToNextId)
+            {
+                int currentGuardId = int.Parse(recordsAsList[i].Value.Substring(1,recordsAsList[i].Value.IndexOf(' ')));
+                int recordsForCurrentIdPeriod = 0;
+                bool idChanged = false;
+
+                while (!idChanged)
+                {
+                    var currentRecord = recordsAsList[i + recordsForCurrentIdPeriod];
+
+                    if (guardsSeparated.ContainsKey(currentGuardId))
+                    {
+                        guardsSeparated[currentGuardId].Add(currentRecord.Key, currentRecord.Value);
+                    }
+
+                    else
+                    {
+                        var recordsForId = new SortedDictionary<DateTime, string>
+                        {
+                            { currentRecord.Key, currentRecord.Value }
+                        };
+
+                        guardsSeparated.Add(currentGuardId, recordsForId);
+                    }
+
+                    if(i + recordsForCurrentIdPeriod + 1 < recordsAsList.Count)
+                    {
+                        recordsForCurrentIdPeriod++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    if (recordsAsList[i + recordsForCurrentIdPeriod].Value.Contains('#'))
+                    {
+                        idChanged = true;
+                        recordsToNextId = recordsForCurrentIdPeriod;
+                        recordsForCurrentIdPeriod = 0;
+                    }                    
+                }
+            }
+            
+            return guardsSeparated;
+        }
+    }
+}
